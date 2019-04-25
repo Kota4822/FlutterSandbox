d3d02aad4b9ee960d85dd17c9816da5acb859577
Kota4822 <clarissa48.22@gmail.com>
2019-04-25 18:14:11 +0900

    初期コードの切り出し

---
+++ lib/DefaultSample/increment.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class IncrementPageIncrementPage extends StatefulWidget {
+
+  IncrementPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _IncrementPageState createState() => _IncrementPageState();
+}
+
+class _IncrementPageState extends State<IncrementPage> {
+
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+}
