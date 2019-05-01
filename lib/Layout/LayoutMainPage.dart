15e37fdb79d974446a63ca53f13c4e110270966c
Kota4822 <clarissa48.22@gmail.com>
2019-05-01 19:56:21 +0900

    Tabbarタップによるstateの変更に対応

--- lib/Layout/LayoutMainPage.dart
+++ lib/Layout/LayoutMainPage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+enum PageType {
+  primary,
+  secondary,
+}
 
 class LayoutMainPage extends StatefulWidget {
 
@@ -10,6 +14,9 @@ class LayoutMainPage extends StatefulWidget {
 }
 
 class _LayoutMainPageState extends State<LayoutMainPage> {
+
+  PageType _pageType = PageType.primary;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +25,24 @@ class _LayoutMainPageState extends State<LayoutMainPage> {
   }
 
   Widget _buildBody() {
+
+    /// 初期値無しで定義可能っぽい？
+    /// flutter: null
+    /// nullが入るっぽい
+    Color _pageColor;
+
+    /// とりあえず色、追ってpageごと挿げ替える
+    switch (_pageType) {
+      case PageType.primary:
+        _pageColor = Colors.pink;
+        break;
+      case PageType.secondary:
+        _pageColor = Colors.amberAccent;
+        break;
+    };
+
     return Scaffold(
+      backgroundColor: _pageColor,
       appBar: AppBar(
         // この辺の改行とインデントの一般解がちょっとわからん
         leading: IconButton(icon: ColorIcon.pink(Icons.star),
@@ -40,8 +64,29 @@ class _LayoutMainPageState extends State<LayoutMainPage> {
           _buildBottomNavigationBarItem(),
           _buildBottomNavigationBarItem()
         ],
-      onTap: null,
+      ///　↓引数書かなくてもいい感じにつないでくれるっぽい,closureちっく
+      onTap: _onSelectTab,
     );
+  }
+
+  /// タブ選択
+  void _onSelectTab(int index) {
+    switch (index) {
+      case 0:
+        _onPageSelected(PageType.primary);
+        break;
+      case 1:
+        _onPageSelected(PageType.secondary);
+        break;
+    };
+  }
+
+  /// ページタイプの変更
+  void _onPageSelected(PageType selection) {
+    /// stateの変更を行いbuildを呼び出す
+    setState(() {
+        _pageType = selection;
+    });
   }
 
   BottomNavigationBarItem _buildBottomNavigationBarItem() {
